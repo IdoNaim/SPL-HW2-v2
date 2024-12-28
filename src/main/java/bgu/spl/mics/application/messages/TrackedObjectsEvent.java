de67eea37027957b1d55b16397d572304fa619b5
IdoNaim <idonai@post.bgu.ac.il>
2024-12-28 17:55:57 +0200

    made trackedObjectsEvent

--- src/main/java/bgu/spl/mics/application/messages/TrackedObjectsEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/TrackedObjectsEvent.java
@@ -2,6 +2,14 @@ package bgu.spl.mics.application.messages;
 
 import bgu.spl.mics.Broadcast;
 import bgu.spl.mics.Event;
+import bgu.spl.mics.application.objects.StampedDetectedObjects;
 
 public class TrackedObjectsEvent implements Event {
+    private StampedDetectedObjects objects;
+    private int detectionTime;
+
+    public TrackedObjectsEvent(StampedDetectedObjects objectsList, int time){
+        this.objects=objectsList;
+        this.detectionTime=time;
+    }
 }
