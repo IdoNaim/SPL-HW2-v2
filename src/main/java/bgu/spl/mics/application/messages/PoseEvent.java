e4d238f7f1eb73a66675693f1c60c274f15fdf2b
IdoNaim <idonai@post.bgu.ac.il>
2024-12-28 18:43:04 +0200

    added empty constructor for terminatedBroadcast

--- src/main/java/bgu/spl/mics/application/messages/PoseEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/PoseEvent.java
@@ -6,7 +6,7 @@ import bgu.spl.mics.application.objects.Pose;
 public class PoseEvent implements Event {
     private Pose pose;
     public PoseEvent(double x, double y, double yaw){
-        this.pose = new Pose(x,y,yaw);
+        this.pose = new Pose(x,y,yaw); //TODO: maybe change this
     }
 
 }
