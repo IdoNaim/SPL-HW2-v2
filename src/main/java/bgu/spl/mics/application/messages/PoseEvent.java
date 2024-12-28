47be8e482b03f531b7a3b2e5954283ad0f940cc8
IdoNaim <idonai@post.bgu.ac.il>
2024-12-28 18:11:37 +0200

    made Pose Event

--- src/main/java/bgu/spl/mics/application/messages/PoseEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/PoseEvent.java
@@ -3,4 +3,13 @@ package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 
 public class PoseEvent implements Event {
+    private double X;
+    private double Y;
+    private double yaw;
+    public PoseEvent(double x, double y, double yaw){
+        this.X =x;
+        this.Y =y;
+        this.yaw=yaw;
+    }
+    
 }
