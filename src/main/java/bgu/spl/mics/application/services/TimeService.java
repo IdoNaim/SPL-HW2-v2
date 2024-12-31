d230f6844bdde019621d9f89477adc259ad7ef25
IdoNaim <idonai@post.bgu.ac.il>
2024-12-31 22:50:53 +0200

    made TimeService send terminatedBroadcast when ending

--- src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -1,6 +1,7 @@
 package bgu.spl.mics.application.services;
 
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.TerminatedBroadcast;
 import bgu.spl.mics.application.messages.TickBroadcast;
 
 /**
@@ -40,6 +41,7 @@ public class TimeService extends MicroService {
                 Thread.sleep(getSpeed());
             }
             terminate();
+            sendBroadcast( new TerminatedBroadcast(getName()));
             Thread.currentThread().interrupt();
         }catch (InterruptedException e){
             e.printStackTrace();
